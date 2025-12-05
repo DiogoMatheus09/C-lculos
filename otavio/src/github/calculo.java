4cc5a6845e07afe3eaef882a91aba75b3dfc35d4
DiogoMatheusdoNascim <DiogoMatheusdoNascim@CEPCACLAB50662>
2025-12-05 17:49:50 -0300

    Adicionado contagem regressiva e formula de bhaskara

--- otavio/src/github/calculo.java
+++ otavio/src/github/calculo.java
@@ -5,11 +5,14 @@ class calculo {
 	public static void main(String[] args) {
 		String a = JOptionPane.showInputDialog(null, "Digite o valor de A", "Entrada de dados", JOptionPane.QUESTION_MESSAGE);
 		String b = JOptionPane.showInputDialog(null, "Digite o valor de B", "Entrada de dados", JOptionPane.QUESTION_MESSAGE);
+		String c = JOptionPane.showInputDialog(null, "Digite o valor de C", "Entrada de dados", JOptionPane.QUESTION_MESSAGE);
+		
 		
 		int a0 = Integer.parseInt(a);
 		int b0 = Integer.parseInt(b);
+		int c0 = Integer.parseInt(c);
 		
-		int valor = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha a operação: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão ", "Operação", JOptionPane.QUESTION_MESSAGE));
+		int valor = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha a operação: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Contagem\n6 - Bhaskara", "Operação", JOptionPane.QUESTION_MESSAGE));
 		switch(valor) {
 		case 1:
 			JOptionPane.showMessageDialog(null, "O símbolo selecionado é soma");
@@ -35,6 +38,31 @@ class calculo {
 				
 			}
 			break;
+		case 5 :
+			for(int i = a0; i>= 0; i--) {
+				JOptionPane.showMessageDialog(null, "Contagem regressiva..." + i);
+			}
+			break;
+			
+		case 6:
+			if(a0 == 0) {
+				JOptionPane.showMessageDialog(null, "Se A = 0, não é uma equacção do 2° grau.", "Erro", JOptionPane.ERROR_MESSAGE);
+				return;
+			} 
+			double delta = b0 * b0 - 4 * a0 * c0;
+			if(delta < 0) {
+				JOptionPane.showMessageDialog(null,  "Delta < 0: não existem raízes reais.", "Resultado", JOptionPane.INFORMATION_MESSAGE);
+			} else if (delta == 0) {
+				double x = -b0 / (2 * a0);
+				JOptionPane.showMessageDialog(null, String.format( "Delta = 0\nRaiz única: x = %.4f", x), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				double sqrt = Math.sqrt(delta);
+				double x1 = (-b0 + sqrt) / (2 * a0);
+				double x2 = (-b0 - sqrt) / (2 * a0);
+				JOptionPane.showMessageDialog(null, String.format("Delta = %.4f\nx1 = %.4f\nx2 = %.4f", delta, x1, x2), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+			}
+			break;
+			
 			default:
 				JOptionPane.showMessageDialog(null, "O código informado é inválido.");
                 break;
